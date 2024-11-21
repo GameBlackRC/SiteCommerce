7809a5827908ff8e78823fb76e41be1ca2fdf11e
Julie Cariou <julie.cariou2605@gmail.com>
2024-11-21 14:55:09 +0100

    merge

--- src/model/commande.js
+++ src/model/commande.js
@@ -1,10 +1,157 @@
-class Commande {
-    client
-    listProduit
-    montant
-    statut
+const CompteService = require('../service/compteService');
+const connection = require('./../../dbConnect');
+const Produit = require('./produit');
+const MD5 = require('crypto-js/md5');
 
-    constructor(statut) {
-        statut = ''
+class Compte {
+    id;
+    login;
+    adresseMail;
+    password;
+    panier = [];
+
+    constructor(id, login, password, adresseMail) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.adresseMail = adresseMail;
     }
+
+    get login() {
+        return this.login;
+    }
+
+    get adresseMail() {
+        return this.adresseMail;
+    }
+
+    get password() {
+        return this.password;
+    }
+
+    get panier() {
+        return this.panier;
+    }
+
+    async remove() {
+        await CompteService.delete(this.id);
+        delete this;
+    }
+
+    static async add(login, password, adresseMail) {
+        const result = await CompteService.add({
+            login: login,
+            password: MD5(password).toString(),
+            adresseMail: adresseMail
+        })
+        const compte = new Compte(result.insertId, login, MD5(password).toString(), adresseMail);
+        return compte;
+    }
+
+    static async removeById(id) {
+        const data = await CompteService.delete(id);
+    }
+
+    static async getById(id) {
+        const data = await CompteService.getById(id);
+        const compte = new Compte(data.id, data.login, data.password, data.adresseMail);
+        await compte.loadPanier();
+        return compte;
+    }
+
+    async loadPanier() {
+        const data = await CompteService.getPanier(this.id);
+        data.forEach((produit) => {
+            this.panier.push(new Produit(produit.nom, produit.urlImage, produit.description, produit.prix, produit.categorie))
+        })
+    }
+
+    get prixPanier() {
+        let total = 0;
+        this.panier.forEach((produit) => {
+            total += produit.prix;
+        })
+        return total;
+    }
+
 }
+
+module.exports = Compteconst CompteService = require('../service/compteService');
+const connection = require('./../../dbConnect');
+const Produit = require('./produit');
+const MD5 = require('crypto-js/md5');
+
+class Compte {
+    id;
+    login;
+    adresseMail;
+    password;
+    panier = [];
+
+    constructor(id, login, password, adresseMail) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.adresseMail = adresseMail;
+    }
+
+    get login() {
+        return this.login;
+    }
+
+    get adresseMail() {
+        return this.adresseMail;
+    }
+
+    get password() {
+        return this.password;
+    }
+
+    get panier() {
+        return this.panier;
+    }
+
+    async remove() {
+        await CompteService.delete(this.id);
+        delete this;
+    }
+
+    static async add(login, password, adresseMail) {
+        const result = await CompteService.add({
+            login: login,
+            password: MD5(password).toString(),
+            adresseMail: adresseMail
+        })
+        const compte = new Compte(result.insertId, login, MD5(password).toString(), adresseMail);
+        return compte;
+    }
+
+    static async removeById(id) {
+        const data = await CompteService.delete(id);
+    }
+
+    static async getById(id) {
+        const data = await CompteService.getById(id);
+        const compte = new Compte(data.id, data.login, data.password, data.adresseMail);
+        await compte.loadPanier();
+        return compte;
+    }
+
+    async loadPanier() {
+        const data = await CompteService.getPanier(this.id);
+        data.forEach((produit) => {
+            this.panier.push(new Produit(produit.nom, produit.urlImage, produit.description, produit.prix, produit.categorie))
+        })
+    }
+
+    get prixPanier() {
+        let total = 0;
+        this.panier.forEach((produit) => {
+            total += produit.prix;
+        })
+        return total;
+    }
+
+}
+
+module.exports = Compte
--- src/route/appRoutes.js
+++ src/route/appRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const AppController = require('../controller/appController.js');
+const CompteController = require('../controller/compteController.js')
 
 const router = express.Router();
 
 router.get("/", AppController.home);
 router.get("/listeProduits", AppController.listeProduits);
 router.get("/login", AppController.login);
+router.get("/commande", AppController.commande);
+router.get("/compte/:id", AppController.compteClient);
+router.get("/panier/:id", AppController.panier);
+// router.post("/modification", CompteController.modification);
 
 module.exports = router;
